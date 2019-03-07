c28c22a30960d3fac829fa4ed3891f69db4a7c88
Asanga Abeyaratne <asabe@prod.main.ntgov>
2019-03-07 16:12:18 +0930

    etl mods

--- ptx-etl.R
+++ ptx-etl.R
@@ -1,14 +1,13 @@
 rm(list=ls())
-#setwd("D:/OneDrive/Projects/PTX/ptx-pt/Proj-PTX")
+
 
 
 library(readxl)
-library(dplyr)
+
 icd <- read_excel("HAR-Fracture.xlsx", col_types = c("text", "date", "text"))
 lab <- read_excel("PATH_lab.xlsx", col_types = c("text", "text", "text","text", "date"))
 dmg <- read_excel("PHC_demo.xlsx", col_types = c("text","date", "text", "date", "text"))
-med <- read_excel("PHC-d3.xlsx", col_types = c("text","text", "date", "numeric"))
-rxd3 <- read_excel("PHC-d3.xlsx", col_types = c("text","blank", "date", "numeric"))
+rxd3 <- read_excel("PHC-d3.xlsx", col_types = c("text","skip", "date", "numeric"))
 
 
 
